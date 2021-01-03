3ac740e0e1dcd2f4239294b2263fc21b934e8d2c
jdhofmann <justindhofmann@gmail.com>
2021-01-03 10:33:39 -0500

    size variation

--- main.js
+++ main.js
@@ -4,13 +4,17 @@ let bkgd = document.querySelector(".background")
 
 const createStars = () => {
     
-    for(let i = 0; i < 300; i++ ){
+    for(let i = 0; i < 200; i++ ){
         let top = (Math.random() * 100).toPrecision(4);
         let left = (Math.random() * 100).toPrecision(4);
+        let size = (Math.random() * 3).toPrecision(3);
         let star = document.createElement("span")
         star.classList.add("star")
         star.style.left = `${left}%`
         star.style.top = `${top}%`
+        star.style.height = `${size}px`
+        star.style.width = `${size}px`
+
         bkgd.append(star)
     }
 
