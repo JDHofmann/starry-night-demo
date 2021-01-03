8b994ab33bcb59346c7a3ce21cda06a29da2b0ef
jdhofmann <justindhofmann@gmail.com>
2021-01-03 10:38:28 -0500

    scss file update

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-let bkgd = document.querySelector(".background")
+let bkgd = document.querySelector(".background");
+let quantity = 200;
 
 
-
-const createStars = () => {
+const createStars = (quantity) => {
     
-    for(let i = 0; i < 200; i++ ){
+    for(let i = 0; i < quantity; i++ ){
         let top = (Math.random() * 100).toPrecision(4);
         let left = (Math.random() * 100).toPrecision(4);
         let size = (Math.random() * 3).toPrecision(3);
         let star = document.createElement("span")
         star.classList.add("star")
+        star.classList.add(`${i}`)
         star.style.left = `${left}%`
         star.style.top = `${top}%`
         star.style.height = `${size}px`
         star.style.width = `${size}px`
-
         bkgd.append(star)
     }
-
-    
 }
 
-createStars();
+createStars(quantity);
