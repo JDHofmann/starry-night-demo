c9f603b3c6f57efd4a626a8c9211ba8b4cef67ea
jdhofmann <justindhofmann@gmail.com>
2021-01-03 18:04:42 -0500

    adding colors in

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let bkgd = document.querySelector(".background");
 let quantity = 200;
-
+let blues = 20
 
 const createStars = (quantity) => {
     
@@ -8,8 +8,17 @@ const createStars = (quantity) => {
         let top = (Math.random() * 100).toPrecision(4);
         let left = (Math.random() * 100).toPrecision(4);
         let size = (Math.random() * 3).toPrecision(3);
-        let star = document.createElement("span")
+        let star = document.createElement("span");
         star.classList.add("star")
+
+        if( i % 3 === 0 ){
+            star.classList.add("blue")
+        } else if ( (i-1)%3 === 0){
+        } else if ( i % 7 === 0){
+            star.classList.add("red")
+
+        }
+
         star.classList.add(`${i}`)
         star.style.left = `${left}%`
         star.style.top = `${top}%`
@@ -18,5 +27,6 @@ const createStars = (quantity) => {
         bkgd.append(star)
     }
 }
+
 
 createStars(quantity);
